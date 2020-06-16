dea06e1187f157c8598854712ebd3c248ae89713
RafaelLindarte <suspect567@gmail.com>
2020-06-16 12:35:24 -0500

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,19 @@
 //------------------
 
 process.env.PORT = process.env.PORT || 3000;
+//------------------
+// Entorno
+//------------------
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//------------------
+// Base de datos
+//------------------
+let urlDB;
+if(process.env.NODE_ENV ==='dev'){
+    urlDB='mongodb://localhost:27017/test';
+}
+else{
+    urlDB='mongodb+srv://Rafael:4yZx2FUo6OsK6OUK@cluster0-pixfd.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+
+}
+process.env.URLDB = urlDB;
