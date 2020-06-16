fbd347c2df906338d932246709297abc1d64014c
RafaelLindarte <suspect567@gmail.com>
2020-06-16 13:32:45 -0500

    Variable de entorno agregada por seguridad-MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ if(process.env.NODE_ENV ==='dev'){
     urlDB='mongodb://localhost:27017/test';
 }
 else{
-    urlDB='mongodb+srv://Rafael:4yZx2FUo6OsK6OUK@cluster0-pixfd.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    urlDB=process.env.MONGO_URI;
 
 }
 process.env.URLDB = urlDB;
